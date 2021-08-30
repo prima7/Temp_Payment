c44445139dc49bebb56a36cd5f5f29cafb8da0c6
Taufik Amaryansyah <taufik.amary@gmail.com>
2021-08-30 13:08:18 +0700

    convert to cra

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import Sidebar from './components/sidebar/SideBar';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Sidebar />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
